40c2a10c67d1d660b48c508a7d7e1894c838ce27
whdqls0289 <44717229+whdqls0289@users.noreply.github.com>
2018-11-03 16:55:53 +0900

    Add files via upload
    day 1

---
+++ Assets/scripts/player/FSMManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Playerstate 
+{ 
+    IDLE = 0,
+    RUN,
+    CHASE,
+    ATTACK
+}
+public class FSMManager : MonoBehaviour
+{
+
+
+    public Playerstate currentState;
+    public Playerstate startState;
+    public Transform Marker;
+
+    Dictionary<Playerstate, PlayerFSMState> states
+        = new Dictionary<Playerstate, PlayerFSMState>();
+
+    private void Awake()
+    {
+        Marker = GameObject.FindGameObjectWithTag("Marker").transform;
+
+        states.Add(Playerstate.IDLE,GetComponent<PlayerIDLE>());
+        states.Add(Playerstate.RUN, GetComponent<PlayerRUN>());
+        states.Add(Playerstate.CHASE, GetComponent<PlayerCHASE>());
+        states.Add(Playerstate.ATTACK, GetComponent<PlayerATTACK>());
+
+        states[startState].enabled = true;
+    }
+
+    public void SetState(Playerstate newstate)
+    {
+        foreach(PlayerFSMState fsm in states.Values)
+        {
+            fsm.enabled = false;
+        }
+        states[newstate].enabled = true;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SetState(startState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(r, out hit, 1000))
+            {
+                Marker.position = hit.point;
+
+                SetState(Playerstate.RUN);
+            }
+        }
+    }
+}
---
+++ Assets/scripts/player/PlayerRUN.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRUN : PlayerFSMState
+{
+    public Transform Marker;
+    public float movespeed = 3.0f;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        Marker = GameObject.FindGameObjectWithTag("Marker").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            Marker.position,
+            movespeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position,Marker.position)<0.01f)
+        {
+            GetComponent<FSMManager>().SetState(Playerstate.IDLE);
+        }
+	}
+}
